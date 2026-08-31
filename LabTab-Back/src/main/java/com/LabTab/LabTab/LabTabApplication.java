66008bbaa31ede7dffe370601db45845898f58b8
IvanCastillo <iacastillo.ili90@gmail.com>
2026-08-30 22:40:24 -0400

    Refactorización: separación de backend, frontend y mobile de la demo MesaSplit, inicio de backend con Java Spring para LabTab v3

---
+++ LabTab-Back/src/main/java/com/LabTab/LabTab/LabTabApplication.java
@@ -0,0 +1,13 @@
+package com.LabTab.LabTab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabTabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabTabApplication.class, args);
+	}
+
+}
---
+++ LabTab-Back/src/test/java/com/LabTab/LabTab/LabTabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LabTab.LabTab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabTabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
