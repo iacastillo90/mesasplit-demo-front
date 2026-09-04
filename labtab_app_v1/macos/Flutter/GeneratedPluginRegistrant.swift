32e5d246530a4e9c2e70fa7dce6c21936fc2631e
IvanCastillo <iacastillo.ili90@gmail.com>
2026-09-03 23:41:04 -0400

    chore(init): agregar directorios de plataforma Linux, macOS, Web, Windows
    Flutter genera archivos de plataforma por defecto.
    Se incluyen para permitir build cross-platform.
    
    - Linux: runner de GTK
    - macOS: runner de Cocoa
    - Web: index.html + manifest
    - Windows: runner de Win32

---
+++ labtab_app_v1/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,24 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import connectivity_plus
+import flutter_local_notifications
+import flutter_secure_storage_macos
+import mobile_scanner
+import sqflite_darwin
+import url_launcher_macos
+import webview_flutter_wkwebview
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
+}
